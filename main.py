62c038e6104ae030080a1b402404ac5cc0a07195
Alex <160585624+rawrbober@users.noreply.github.com>
2025-03-26 03:19:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from overlay import OsuOverlay
 from get_ID_and_mods import GetStart
+import keyboard
+import time
+from manualdata import mandata
 
-HOTKEY = 'f4'
 TIME_TO_SLEEP = 0.03
-TAB_LOADING_TIME = 0.08
-# This will automatically minimize the browser after opening a map
-# To enable, put in your own path, to disable, set to: None
-BROWSER_TO_MINIMIZE = 'Chrome'
+SONG_PATH = "D:/osu!/Songs"
 
+ 
 def main():
     # Main function to run the application loop
     while True:
-        # Get map and mods details and prepare initialization
-        start_mods_and_mapID = GetStart(TIME_TO_SLEEP, TAB_LOADING_TIME, HOTKEY, BROWSER_TO_MINIMIZE)
-        mods = start_mods_and_mapID.start_hotkeys()
-        overlay = OsuOverlay(mods)
-        # Run the canvas and circle rendering script
+        keyboard.unhook_all()
+        handler = GetStart(TIME_TO_SLEEP, SONG_PATH)
+        mods = handler.start_hotkeys()
+        data = handler.get_map_data()
+        time.sleep(1)
+        print("Throwing it into overlay")
+        overlay = OsuOverlay(data,mods)
         overlay.initialize_script()
+
 
 if __name__ == '__main__':
     main()
+
